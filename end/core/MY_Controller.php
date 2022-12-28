e3403765713059a57cb7aa1540a4fcc48a2899d2
fanzhe <fanzhe@yibaicloud.com>
2022-12-28 16:08:47 +0800

    v1

---
+++ end/core/MY_Controller.php
@@ -0,0 +1,8 @@
+<?php (defined('BASEPATH')) or exit('No direct script access allowed');
+
+/* load the MX_Loader class */
+require APPPATH . 'third_party/MX/Controller.php';
+
+class MY_Controller extends MX_Controller
+{
+}
